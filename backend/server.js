07241f97ab31544ed91e1d25d22f2abaaa88de17
Betelhem Yehuala <betelyehuala@gmail.com>
2026-07-03 00:53:47 +0300

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -23,8 +23,12 @@ app.use(cors({
     "http://localhost:3000",
     "https://backend-iy4z.onrender.com"
   ],
+
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
+app.options("*", cors());
 app.use(express.json());
 app.use('/uploads', express.static(uploadDir));
 
